3573f4be66981cf0e7c1a9cb9bd0dc7cbe6e9258
Azizi <munaemazizi@gmail.com>
2022-08-29 11:47:32 +0430

    Add modified files

---
+++ JS.js
@@ -0,0 +1,48 @@
+
+
+
+
+// var x = 10;
+
+// if(x==10){
+// /* doccumnt.wite('x==10')
+// dadf asdfkkmg;dkjfkjd
+
+// fif */ addEventListener; alert("Hi")
+
+// }else{
+//     document.write('x !=10')
+// }
+
+// var x = 30;
+// var y = 20;
+
+// var z = x + y;
+
+// document.write(z)
+
+// document.getElementById(total).innerHTML="total";
+
+// document.getElementById('demo').innerHTML="Hello Everyone";
+
+// var x = 200;
+// var y = 40;
+// var z = x + y;
+
+// document.getElementById('Hi').innerHTML="Addition of x,y:"+z;
+
+// var x = 120;
+// var y = 8;
+// var z = x + y;
+// document.getElementById('Add').innerHTML="Addition of x,y:"+z;
+
+// var x = 120;
+// var y = 8;
+// var z = x - y;
+// document.getElementById('Sub').innerHTML="Subtraction of x,y:"+z;
+
+
+// var x = 120;
+// var y = 8;
+
+// document.getElementById('Ternary-Operator').innerHTML=(y > x) ? "y is greater x":"x is greater y";
